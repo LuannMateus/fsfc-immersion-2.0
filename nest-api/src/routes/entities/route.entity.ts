38ac83ba679192b2bdb071380494d7015599858f
LuannMateus <luanmateuslima34@gmail.com>
2021-04-06 16:49:21 -0300

    Second day - Nest API

---
+++ nest-api/src/routes/entities/route.entity.ts
@@ -0,0 +1,30 @@
+import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+type RouteDocument = Route & Document;
+
+@Schema()
+class Route {
+  @Prop()
+  title: string;
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  startPosition: { lat: number; lng: number };
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  endPosition: { lat: number; lng: number };
+}
+
+const RouteSchema = SchemaFactory.createForClass(Route);
+
+export { Route, RouteDocument, RouteSchema };
